5563a8121af8b175115a054e4bb9b8acfd295440
Fernando Marin <fmarin@s30labs.com>
2018-07-20 18:03:12 +0200

    [IMP] linux_metric_file_server.pl -> more parameters -> designer changes

--- designer/proxy-pkgs/linux_metric_file_server.pm
+++ designer/proxy-pkgs/linux_metric_file_server.pm
@@ -56,7 +56,8 @@ $linux_metric_file_server::SCRIPT_NAME = 'linux_metric_file_server.pl';
 		'p08' => { '__HPARAM__' => '', '__PARAM_TYPE__' => '0', '__PARAM_PREFIX__' => '-size', '__PARAM_DESCR__' => 'Files Total Size', '__PARAM_VALUE__' => '', '__SCRIPT__' => $linux_metric_file_server::SCRIPT_NAME },
 		'p09' => { '__HPARAM__' => '', '__PARAM_TYPE__' => '0', '__PARAM_PREFIX__' => '-lapse', '__PARAM_DESCR__' => 'Time offset', '__PARAM_VALUE__' => '', '__SCRIPT__' => $linux_metric_file_server::SCRIPT_NAME },
 		'p10' => { '__HPARAM__' => '', '__PARAM_TYPE__' => '0', '__PARAM_PREFIX__' => '-remotedir', '__PARAM_DESCR__' => 'Remote directory', '__PARAM_VALUE__' => '', '__SCRIPT__' => $linux_metric_file_server::SCRIPT_NAME },
-		'p11' => { '__HPARAM__' => '', '__PARAM_TYPE__' => '0', '__PARAM_PREFIX__' => '-timeout', '__PARAM_DESCR__' => 'Timeout', '__PARAM_VALUE__' => '20', '__SCRIPT__' => $linux_metric_file_server::SCRIPT_NAME },
+		'p11' => { '__HPARAM__' => '', '__PARAM_TYPE__' => '0', '__PARAM_PREFIX__' => '-pattern', '__PARAM_DESCR__' => 'Count Group Pattern', '__PARAM_VALUE__' => '', '__SCRIPT__' => $linux_metric_file_server::SCRIPT_NAME },
+		'p12' => { '__HPARAM__' => '', '__PARAM_TYPE__' => '0', '__PARAM_PREFIX__' => '-timeout', '__PARAM_DESCR__' => 'Timeout', '__PARAM_VALUE__' => '20', '__SCRIPT__' => $linux_metric_file_server::SCRIPT_NAME },
 
 		},
 
@@ -74,22 +75,42 @@ action=test
 <002RC> STATUS - File Transfer Success (%) = 0
 
 action=count
-<003> Number of Files = unk
-<003RC> STATUS - Number of Files = 0
-
-action=last
-<004> Last File Modification Time = 118224
-<004RC> STATUS - Last File Modification Time = 0
+<003> Total Files = 8587
+<003.CM_> Total Files (CM_) = 1454
+<003.MP_> Total Files (MP_) = 2565
+<003.SS_> Total Files (SS_) = 1988
+<003.ST_> Total Files (ST_) = 2580
+<003RC> STATUS - Total Files = 0
+<004> Latest File Modification Time = 1532026296
+<004RC> STATUS - Latest File Modification Time = 0
+<005> Oldest File Modification Time = 1529624299
+<005RC> STATUS - Oldest File Modification Time = 0
+<006> Total Files before lapse = 8559
+<006.CM_> Total Files before lapse (CM_) = 1449
+<006.MP_> Total Files before lapse (MP_) = 2557
+<006.SS_> Total Files before lapse (SS_) = 1981
+<006.ST_> Total Files before lapse (ST_) = 2572
+<006RC> STATUS - Total Files before lapse = 0
+<007> Total Files after lapse = 28
+<007.CM_> Total Files after lapse (CM_) = 5
+<007.MP_> Total Files after lapse (MP_) = 8
+<007.SS_> Total Files after lapse (SS_) = 7
+<007.ST_> Total Files after lapse (ST_) = 8
+<007RC> STATUS - Total Files after lapse = 0
 
 File Transfer Latency show the time consumed in the test overall operation.
 File Transfer Success show the percent of success in the test file transfers.
-Number of Files counts the files on directory
-Last File Modification Time shows the delta time with the current time
+Total Files counts the number of files in directory
+Latest File Modification Time shows the most recent file time in directory
+Oldest File Modification Time shows the oldest file time in directory
+Total Files before lapse counts the number of files in directory before the specified lapse
+Total Files after lapse counts the number of files in directory after the specified lapse
 The parameters are:
 
  linux_metric_file_server.pl -host 1.1.1.1 -user user1 -pwd xxx [-port 22|...] [-proto sftp|...] [-action test] [-files 10|...] [-size 100000|...] [-v]
  linux_metric_file_server.pl -host 1.1.1.1 -user user1 -pwd xxx [-port 22|...] [-proto sftp|...] -action count [-remotedir /dir1/xx] [-v]
- linux_metric_file_server.pl -host 1.1.1.1 -user user1 -pwd xxx [-port 22|...] [-proto sftp|...] -action last [-remotedir /dir1/xx] [-lapse 3600] [-v]
+ linux_metric_file_server.pl -host 1.1.1.1 -user user1 -pwd xxx [-port 22|...] [-proto sftp|...] -action count [-remotedir /dir1/xx] [-lapse 3600] [-v]
+ linux_metric_file_server.pl -host 1.1.1.1 -user user1 -pwd xxx [-port 22|...] [-proto sftp|...] -action count [-remotedir /dir1/xx] [-lapse 3600]  [-pattern a,b,c] [-v]
  linux_metric_file_server.pl -h  : Help
 
  -host       : File Server Host
@@ -97,11 +118,12 @@ The parameters are:
  -user       : Server User
  -pwd        : Server User Password
  -proto      : Protocol (default sftp)
- -action     : test|count|last
+ -action     : test|count
  -files      : Number of files used (tx/rx). Default is 10. (mode=test)
  -size       : Aggregated size. Default is 300KB. (mode=test)
  -remotedir  : Remote directory  (mode=count|test)
- -lapse      : Tnow-lapse  (mode=count)
+ -lapse      : Defines a time reference (tRef) = Tnow-lapse in mode=count.
+ -pattern    : Text pattern (or patterns) for file classification in mode count
  -timeout    : Max. Timeout [Default 20 sg]
  -v/-verbose : Verbose output (debug)
  -h/-help    : Help
