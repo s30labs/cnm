e8c6880144d5ad11bf363a49f96d4c4463b65573
Fernando Marin <fmarin@s30labs.com>
2020-03-17 20:34:26 +0100

    [IMP] Syntax error  in DB-Scheme-Init-CNM-cnms.php with CNM_LOCAL_IP

--- update/db/Init-cnm/cfg_cnms/DB-Scheme-Init-CNM-cnms.php
+++ update/db/Init-cnm/cfg_cnms/DB-Scheme-Init-CNM-cnms.php
@@ -2,7 +2,7 @@
 $iface = 'eth0';
 $file = '/cfg/onm.if';
 if(file_exists($file) and false!=file_get_contents($file)) $iface = chop(file_get_contents($file));
-if (getenv("CNM_LOCAL_IP") !== false) { $local_ip = getenv("CNM_LOCAL_IP"); }
+if (getenv("CNM_LOCAL_IP") !== false) { $ip = getenv("CNM_LOCAL_IP"); }
 else {
 	$ip = chop(`/sbin/ifconfig $iface|grep 'inet addr'|cut -d ":" -f2|cut -d " " -f1`);
 }
