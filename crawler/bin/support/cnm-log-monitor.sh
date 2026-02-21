4e36a9d4f259445bcc97cd84ee11df9d5bac0c81
Fernando Marin <fmarin@s30labs.com>
2026-02-21 12:32:36 +0100

    [IMP] Improvements in support/cnm-log-monitor.sh

--- crawler/bin/support/cnm-log-monitor.sh
+++ crawler/bin/support/cnm-log-monitor.sh
@@ -28,34 +28,21 @@ reiniciar_proceso() {
 # Función para procesar un archivo de log
 procesar_log() {
     local LOG_FILE="$1"
-    
-    # Obtener la hora actual y la hora hace 30 minutos
-    local CURRENT_HOUR=$(date +"%H")
-    local CURRENT_MINUTE=$(date +"%M")
-    
-    # Determinar el intervalo de búsqueda
-    if [ "$CURRENT_MINUTE" -eq "00" ]; then
-        local START_HOUR=$(date -d "1 hour ago" +"%H")
-        local START_MIN=30
-    else
-        local START_HOUR=$CURRENT_HOUR
-        local START_MIN=00
-    fi
-    
-    # Construir el patrón de tiempo para grep
-    local TIME_PATTERN1=$(date +"%b %e $START_HOUR:$START_MIN")
-    local TIME_PATTERN2=$(date +"%b %e $CURRENT_HOUR:$CURRENT_MINUTE")
-    
-    #echo "Procesando $LOG_FILE - Buscando líneas entre $TIME_PATTERN1:00 y $TIME_PATTERN2:59 con texto '$SEARCH_TEXT'"
-	 echo "cnm-log-monitor.sh Parsing $LOG_FILE between $TIME_PATTERN1:00 and $TIME_PATTERN2:59 with text '$SEARCH_TEXT'"
-    logger -p user.notice "cnm-log-monitor.sh Parsing $LOG_FILE between $TIME_PATTERN1:00 and $TIME_PATTERN2:59 with text '$SEARCH_TEXT'"
+    local NUM_LINES=10000
 
-    # Buscar la primera línea con el texto especificado en el rango de tiempo
-    local MATCHED_LINE=$(grep -E "$TIME_PATTERN1|$TIME_PATTERN2" "$LOG_FILE" | grep -w "$SEARCH_TEXT_ESC" | head -n 1)
-    
+    echo "cnm-log-monitor.sh Parsing last $NUM_LINES lines of $LOG_FILE with text '$SEARCH_TEXT'"
+    logger -p user.notice "cnm-log-monitor.sh Parsing last $NUM_LINES lines of $LOG_FILE with text '$SEARCH_TEXT'"
+
+    # Buscar la primera línea que contenga el texto literal
+    local MATCHED_LINE=$(tail -n "$NUM_LINES" "$LOG_FILE" | grep -F "$SEARCH_TEXT" | head -n 1)
+
+    echo "$MATCHED_LINE"
+
     if [[ -n "$MATCHED_LINE" ]]; then
+        #echo "REINICIO PROCESO 0"
         if [[ "$MATCHED_LINE" =~ \[([^\]]+)\]\[([0-9]+)\] ]]; then
-			   reiniciar_proceso "${BASH_REMATCH[1]}" "${BASH_REMATCH[2]}"
+            #echo "REINICIO PROCESO 1"
+            reiniciar_proceso "${BASH_REMATCH[1]}" "${BASH_REMATCH[2]}"
         fi
     fi
 }
